f585bbd0a9c9d78a8f09a9d6f3d78c2b5a5aabba
Nathan Galindo <nathan.zebedee@gmail.com>
2022-10-27 10:48:21 -0500

    revised fizzbuss

--- week-01/code/fizzbuzz/src/starter.rs
+++ week-01/code/fizzbuzz/src/starter.rs
@@ -4,8 +4,8 @@ pub mod starter {
     use super::*;
 
     // define a public function called "starter" that returns a Counts struct
-        // Counts { fizz_count: i32, buzz_count: i32, fizzbuzz_count: i32 }
-        // loop through 1 to 100
+        // Counts { fizzbuzz: 0, fizz: 0, buzz: 0 }
+        // loop through 1 to 100 (inclusive)
             // if the number is divisible by 3 and 5, increment fizzbuzz_count
             // if the number is divisible by 3, increment fizz_count
             // if the number is divisible by 5, increment buzz_count
@@ -13,9 +13,9 @@ pub mod starter {
     pub fn starter() -> Counts {
 
         Counts {
+            fizzbuzz_count: 0,
             fizz_count: 0,
             buzz_count: 0,
-            fizzbuzz_count: 0,
         }
     }
 }
